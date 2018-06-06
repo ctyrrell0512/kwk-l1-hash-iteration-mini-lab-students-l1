4bf2c79e7ac6fbec16ef59d01d243f974169b45b
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-06 22:22:54 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -9,7 +9,7 @@ def add_a_key_value_pair
   summer_olympics = {"Sydney"=> 2000, "Athens"=>2004, "Beijing"=>2008, "London" =>2012}
   new_city = "Atlanta"
   new_year = 1996
-  summer_olympics[new_city]= new_year
+  summer_olympics[new_city] =new_year
 end
 
 add_a_key_value_pair
