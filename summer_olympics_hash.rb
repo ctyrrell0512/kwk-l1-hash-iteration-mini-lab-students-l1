7b502ffd0a5d3c374e55ed34e9d033a9783e6e60
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-06 22:21:27 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -21,6 +21,7 @@ def iterate_through_hash
   summer_olympics[new_city]=new_year
  summer_olympics.each do |olympics|
    puts "The #{olympics.keys} summer olympics took place in #{olympics.value}."
+  end
 end
 
 #iterate_through_hash
@@ -32,6 +33,7 @@ def iterate_through_keys
   summer_olympics[new_city]=new_year
   summer_olympics.each do |olympics|
    puts "The #{olympics.key} summer olympics took place in #{olympics.value}."
+  end  
    summer_olympics.keys.upcase
 end
 
