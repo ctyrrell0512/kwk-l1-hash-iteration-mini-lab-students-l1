737ee96196302fb265895bf825d9f554b59ec2cb
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-06 22:07:54 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -8,15 +8,38 @@ create_olympics_hash
 
 def add_a_key_value_pair
   summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
-  
+  new_city = :Atlanta
+  new_year = 1996
+  summer_olympics[new_city]=new_year
 end
+
+#add_a_key_value_pair
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+ summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
+ new_city = :Atlanta
+  new_year = 1996
+  summer_olympics[new_city]=new_year
+ summer_olympics.each do |olympics|
+   puts "The #{olympics.key} summer olympics took place in #{olympics.value}."
 end
 
+#iterate_through_hash
+
 def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
+  new_city = :Atlanta
+  new_year = 1996
+  summer_olympics[new_city]=new_year
+  summer_olympics.each do |olympics|
+   puts "The #{olympics.key} summer olympics took place in #{olympics.value}."
+   summer_olympics.upcase
 end
+
+#iterate_through_keys
+
+
+
+
+
+
