620a9911c2104b3fae7759b7bca8fb5b9872cedf
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-06 22:11:48 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,13 +1,13 @@
 def create_olympics_hash
-  summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
+  summer_olympics = {"Sydney"=> 2000, "Athens"=>2004, "Beijing"=>2008, "London" =>2012}
   puts summer_olympics
 end
 
 create_olympics_hash
 
 def add_a_key_value_pair
-  summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
-  new_city = :Atlanta
+  summer_olympics = {"Sydney"=> 2000, "Athens"=>2004, "Beijing"=>2008, "London" =>2012}
+  new_city = "Atlanta"
   new_year = 1996
   summer_olympics[new_city]=new_year
 end
@@ -15,8 +15,8 @@ end
 add_a_key_value_pair
 
 def iterate_through_hash
- summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
- new_city = :Atlanta
+ summer_olympics = {"Sydney"=> 2000, "Athens"=>2004, "Beijing"=>2008, "London" =>2012}
+  new_city = "Atlanta"
   new_year = 1996
   summer_olympics[new_city]=new_year
  summer_olympics.each do |olympics|
@@ -26,8 +26,8 @@ end
 iterate_through_hash
 
 def iterate_through_keys
-  summer_olympics = { :Sydney => 2000, :Athens =>2004, :Beijing =>2008, :London =>2012}
-  new_city = :Atlanta
+  summer_olympics = {"Sydney"=> 2000, "Athens"=>2004, "Beijing"=>2008, "London" =>2012}
+  new_city = "Atlanta"
   new_year = 1996
   summer_olympics[new_city]=new_year
   summer_olympics.each do |olympics|
